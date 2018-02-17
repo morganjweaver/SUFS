2266c8f86bd0f9c1eb83bf1c4d085c5c4881ec79
Morgan Weaver <morganjweaver@gmail.com>
2018-02-16 23:43:29 -0800

    added skeleton for Block class and header

---
+++ Block.h
@@ -0,0 +1,11 @@
+#ifndef Block_h
+#define B;ock_h
+#include <string>
+
+class Block [
+
+public:
+
+]
+
+#endif
---
+++ DataNode.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iterator>
+#include <stdio.h>
+
+using namespace std;
+
+//TODO: 
+// Create listener for incoming requests
+// Create handler for incoming requests from listener
+// Create Block class?
+// Create update function to send dir info to NameNode
+// Create DeleteBlock
+// Create StoreBlock
+// 
+
