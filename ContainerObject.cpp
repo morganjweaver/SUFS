7a12b92fe64cefb0b18137f9d2aa1b536ff4091e
Morgan Weaver <morganjweaver@gmail.com>
2018-02-27 15:17:17 -0800

    refactored file.dir objects into family, added socket code to Name and Data node classes

---
+++ ContainerObject.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <iterator>
+#include <stdio.h>
+
+using namespace std;
+
+class ContainerObject
+{
+  public:
+    string name;
+    string path;    
+
+};
+
+class File : public ContainerObject
+{
+  public:  
+    float size;
+    vector<block> blocks;
+   
+};
+
+class Directory : public ContainerObject
+{
+  public:
+    vector<File> files;
+    vector<Directory> directories;
+
+};
+ 
