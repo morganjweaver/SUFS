6ab06d364bdeb52a8d6428af659b013863eedacd
Morgan Weaver <morganjweaver@gmail.com>
2018-03-09 13:33:49 -0800

    cleaning up DataNode again

--- DataNode.cpp
+++ DataNode.cpp
@@ -23,74 +23,15 @@ using namespace std;
 #include <string.h>
 #define PORT 8080
 #define MAXBUF 32
+const int MAXPENDING = 99;
+
 //SERVER SOCKET CODE:
 void receiveBlock(int clientSock);
 string receiveBlockHelper(int sock, string file_name, long file_size);
 string receiveString(int sock);
 long receiveLong(int clientSock);
 
-// int main(int argc, char const *argv[])
-// {
-//     int server_fd, new_socket, valread;
-//     struct sockaddr_in address;
-//     int opt = 1;
-//     int addrlen = sizeof(address);
-//     char buffer[MAXBUF] = {0};
-//     char *hello = "Hello from server";
-      
-//     // Creating socket file descriptor
-//     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-//     {
-//         perror("socket failed");
-//         exit(EXIT_FAILURE);
-//     }
-      
-//     // Forcefully attaching socket to the port 8080
-//     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-//                                                   &opt, sizeof(opt)))
-//     {
-//         perror("setsockopt");
-//         exit(EXIT_FAILURE);
-//     }
-//     address.sin_family = AF_INET;
-//     address.sin_addr.s_addr = INADDR_ANY;
-//     address.sin_port = htons( PORT );
-      
-//     // Forcefully attaching socket to the port 8080
-//     if (bind(server_fd, (struct sockaddr *)&address, 
-//                                  sizeof(address))<0)
-//     {
-//         perror("bind failed");
-//         exit(EXIT_FAILURE);
-//     }
-//     if (listen(server_fd, 3) < 0)
-//     {
-//         perror("listen");
-//         exit(EXIT_FAILURE);
-//     }
-//     if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
-//                        (socklen_t*)&addrlen))<0)
-//     {
-//         perror("accept");
-//         exit(EXIT_FAILURE);
-//     }
-//     ofstream outfile ("testOUTFILE.txt");
-//     while((valread = recv(new_socket, buffer, MAXBUF, 0)) > 0){
-//         if(valread <= 0)
-//             break;
-//     else {
-//         buffer[valread] = '\0'; 
-//         outfile << buffer; 
-//         printf("%s", buffer);
-//     }
-// }
-//     outfile.close();
-//    // valread = read( new_socket , buffer, 1024);
-//    // printf("%s\n",buffer );
-//     send(new_socket , hello , strlen(hello) , 0 );
-//     printf("Hello message sent\n");
-//     return 0;
-// }
+
 int main(int argc, char const *argv[])
 {
   if(argc < 2) {
