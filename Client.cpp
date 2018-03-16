c782f64aefd516336d6deb31a1b2d850ec8db051
Morgan Weaver <morganjweaver@gmail.com>
2018-03-15 17:40:35 -0700

    made client send port flexible again

--- Client.cpp
+++ Client.cpp
@@ -314,7 +314,7 @@ void create(string name, string path, string S3_file, string S3_bucket, int sock
   }
   //string cleanoutsocket = receiveString(socket);
   //string getStringPort = receiveString(socket);
-  string getStringPort = str(NameNodePort);//"8081";
+  string getStringPort (NameNodePort);//"8081";
   cout << "DataNode stats: \n" << "Port: " << getStringPort << endl;
   cout << "Base Name: " << baseName << endl;// << "\nDataNodes to send blocks to: " << IPs << endl;
   
