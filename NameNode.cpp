e28aec2a9932f77fa2951519e1722940e8e0ac09
Hunter Garrett <garretth@seattleu.edu>
2018-03-15 22:29:45 -0700

    Update NameNode.cpp

--- NameNode.cpp
+++ NameNode.cpp
@@ -118,7 +118,8 @@ int main(int argc, char const *argv[])
         struct sockaddr_storage addr;
         char ipstr[INET6_ADDRSTRLEN];
           int port;
-	ipstr[ipstr.length()] = '\0';
+	size_t tempSize = sizeof(ipstr);
+	ipstr[tempSize] = '\0';
 
         len = sizeof addr;
         getpeername(clientSock, (struct sockaddr*)&addr, &len);
@@ -399,7 +400,6 @@ void processClient(int clientSock, string clientIP)
       	cout << getPath << endl;
       	vector<CatObject> catFile;
       	catFile = cat(getPath);
-        cout << "sending NUMBER for cat: " << catFile.size();
       	sendLong(clientSock, catFile.size());
       	for(int i = 0; i < catFile.size(); i++)
       		sendString(clientSock, catFile[i].chunk_ID);
