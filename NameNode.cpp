c2491e7ea2a7653b218d66ce4622e21c21e2ec01
Hunter Garrett <garretth@seattleu.edu>
2018-03-15 21:54:37 -0700

    Update NameNode.cpp

--- NameNode.cpp
+++ NameNode.cpp
@@ -364,6 +364,8 @@ void processClient(int clientSock, string clientIP)
 		      tempstr = tempstr + " " + myStats[i].repIP[j];
 	      statInfo.push_back(tempstr);
       }
+      for(int i = 0; i < statInfo.size(); i++){
+	      statInfo[i] = statInfo[i] + '\0';
       sendLong(clientSock, statInfo.size());
       for(int i = 0; i < statInfo.size(); i++){
         sendString(clientSock, statInfo[i]);
