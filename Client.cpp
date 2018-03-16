9546048f29093eca9ef70e8c5163c59822cfc31c
Morgan Weaver <morganjweaver@gmail.com>
2018-03-15 17:25:55 -0700

    working theory dirty socket

--- Client.cpp
+++ Client.cpp
@@ -314,10 +314,11 @@ void create(string name, string path, string S3_file, string S3_bucket, int sock
       cout << IPs[j] << endl;
   }
   //string cleanoutsocket = receiveString(socket);
-  string getStringPort = receiveString(socket);
-
+  //string getStringPort = receiveString(socket);
+  string getStringPort = "8081";
   cout << "DataNode stats: \n" << "Port: " << getStringPort << endl;
   cout << "Base Name: " << baseName << endl;// << "\nDataNodes to send blocks to: " << IPs << endl;
+  
   unsigned short dataNodePort = (unsigned short)stoi(getStringPort);
 
   vector<string> blockIDnames;
